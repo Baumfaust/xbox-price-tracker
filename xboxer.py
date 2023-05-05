82d275a2e469261535aecdaf125201adb7ea9d97
Baumfaust <baumfaust@gmx.com>
2023-05-05 09:18:29 +0200

    added logger

--- xboxer.py
+++ xboxer.py
@@ -2,15 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 from datetime import datetime
-import time
 from re import sub
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 
 bot_token = os.getenv("BOT_TOKEN")
 chat_id = os.getenv("CHAT_ID")
+
+# Set up logging
+logging.basicConfig(filename='price_tracker.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299'
@@ -20,17 +23,18 @@ url = 'https://www.microsoft.com/de-de/store/collections/xboxconsoles'
 response = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(response.text, 'html.parser')
-device = 0
 
 conn = sqlite3.connect('prices.db')
 c = conn.cursor()
 
 # Create a table to store the prices
 c.execute('''CREATE TABLE IF NOT EXISTS prices
-             (xbox_pid TEXT PRIMARY KEY,
-              name TEXT,
-              price INTEGER,
-              date TEXT)''')
+            (id INTEGER PRIMARY KEY,
+             xbox_pid TEXT,
+             name TEXT,
+             price INTEGER,
+             date TEXT)
+            ''')
 
 def send_telegram_message(bot_token, chat_id, message):
     if bot_token is not None and chat_id is not None:
@@ -41,10 +45,7 @@ def send_telegram_message(bot_token, chat_id, message):
 elements = soup.findAll(attrs={'data-bi-pid': True})
 for xbox_element in elements:
     xbox_pid= xbox_element.get('data-bi-pid')
-    xbox_name = xbox_element.get('data-bi-prdname')
-
-    with open(str(device)+'.html', 'w', encoding='utf-8') as file:
-        file.write(xbox_element.text)
+    xbox_name = xbox_element.get('data-bi-prdname').strip()
 
     price_element = xbox_element.find('p', {'class': 'sr-only'})
 
@@ -57,9 +58,9 @@ for xbox_element in elements:
         if last_price is not None and last_price[0] != xbox_price:
             message = f'Price for {xbox_name} has changed from {last_price[0]/100:.2f} to {xbox_price/100:.2f}'
             send_telegram_message(bot_token, chat_id, message)
-            print(message)
+            logging.info(message)
         else:
-            print(f'Price for {xbox_name} is {xbox_price/100:.2f}')
+            logging.info(f'Price for {xbox_name} is {xbox_price/100:.2f}')
 
         # Insert the new price into the database
         date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
